61a5ef3d406a2f09e4301183f021316d36234dfd
mattmcw <hi@mmcwilliams.com>
2021-11-30 14:47:57 -0500

    Build mac

--- build.sh
+++ build.sh
@@ -6,6 +6,8 @@
 mkdir -p releases
 mkdir -p releases/linux
 mkdir -p releases/windows
+mkdir -p releases/mac
 
 mv application.linux* releases/linux/
 mv application.windows* releases/windows/
+mv application.macosx releases/mac/
